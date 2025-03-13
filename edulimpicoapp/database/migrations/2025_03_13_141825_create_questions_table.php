1f35607567a34320d18ff64e1a3d6f223e4d1f7f
PedroEduardoSS <pedroeduardossousa@gmail.com>
2025-03-13 15:35:25 -0300

    Add dev branch
    Launch dev branch with default migrations

---
+++ edulimpicoapp/database/migrations/2025_03_13_141825_create_questions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('room_id');
+            $table->text('question');
+            $table->text('option_a');
+            $table->text('option_b');
+            $table->text('option_c');
+            $table->text('option_d');
+            $table->enum('answer', ['a', 'b', 'c', 'd']);
+            $table->integer('points');
+            $table->foreign('room_id')->references('id')->on('rooms');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('questions');
+    }
+};
